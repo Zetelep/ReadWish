495db0741f180a194366b5e75ba7eadbc4df8dd7
Zetelep <backup02izul@gmail.com>
2025-04-29 18:40:22 +0800

    finished list more ui for home, create favorite fragment ui, setup for search fragment

--- app/src/main/java/com/zulfa/readwish/presentation/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/zulfa/readwish/presentation/favorite/FavoriteFragment.kt
@@ -1,17 +1,16 @@
-package com.zulfa.readwish.presentation.dashboard
+package com.zulfa.readwish.presentation.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.zulfa.readwish.databinding.FragmentDashboardBinding
+import com.zulfa.readwish.databinding.FragmentFavoriteBinding
 
-class DashboardFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +21,12 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
---
+++ app/src/main/java/com/zulfa/readwish/presentation/list/ListMoreViewModel.kt
@@ -0,0 +1,4 @@
+package com.zulfa.readwish.presentation.list
+
+class ListMoreViewModel {
+}
